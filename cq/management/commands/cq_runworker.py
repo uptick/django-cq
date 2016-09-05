81298dbf31691890f308d1a63b8acfbb3eb96873
Luke Hodkinson <furious.luke@gmail.com>
2016-09-05 12:06:36 +1000

    Overloading the runworker command to make it easier to specify how many web workers vs background task workers there are.

---
+++ cq/management/commands/cq_runworker.py
@@ -0,0 +1,27 @@
+from django.core.management.base import CommandError
+from channels.management.commands.runworker import Command as BaseCommand
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        super().add_arguments(parser)
+        parser.add_argument(
+            '--web-threads', action='store',
+            type=int, help='Number of threads to execute.'
+        )
+        parser.add_argument(
+            '--worker-threads', action='store',
+            type=int, help='Number of threads to execute.'
+        )
+
+    def handle(self, *args, **options):
+        web_threads = options.get('web_threads', 0)
+        wkr_threads = options.get('worker_threads', 0)
+        if (web_threads or wkr_threads):
+            options['threads'] = web_threads + wkr_threads
+            if web_threads:
+                if options.get('thread_only_channels', None) is None:
+                    options['thread_only_channels'] = []
+                for ii in range(web_threads):
+                    options['thread_only_channels'].append('%d,http.*' % ii)
+        super().handle(*args, **options)
