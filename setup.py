5827855b55dbc134b94acb043e9fee21fb9d70e5
Luke Hodkinson <furious.luke@gmail.com>
2016-10-28 10:13:17 +1100

    Bump version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-cq',
-    version='0.1.9',
+    version='0.1.10',
     author='Luke Hodkinson',
     author_email='furious.luke@gmail.com',
     maintainer='Luke Hodkinson',
