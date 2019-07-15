986c9052400585bb42ea7df278213189f939c734
andrew <andrew.ang@uptickhq.com>
2019-07-15 14:14:08 +1000

    Just wrap in try/except

--- cq/apps.py
+++ cq/apps.py
@@ -3,19 +3,10 @@ import logging
 from django.apps import AppConfig
 from django.conf import settings
 from django.core.cache import cache
-from django.db.backends.signals import connection_created
-from django.db.models.signals import post_migrate
+from django.db.utils import ProgrammingError
 from django.utils.module_loading import import_module
 
 logger = logging.getLogger('cq')
-
-
-migration_happened = False
-
-
-def post_migration_callback(sender, **kwargs):
-    global migration_happened
-    migration_happened = True
 
 
 def scan_tasks(*args, **kwargs):
@@ -26,12 +17,17 @@ def scan_tasks(*args, **kwargs):
             pass
 
 
-def requeue_tasks(sender, connection, *args, **kwargs):
+def requeue_tasks(*args, **kwargs):
     from cq.models import Task
     lock = 'RETRY_QUEUED_TASKS'
     with cache.lock(lock, timeout=2):
         # Find all Queued tasks and set them to Retry, since they get stuck after a reboot
-        Task.objects.filter(status=Task.STATUS_QUEUED).update(status=Task.STATUS_RETRY)
+        try:
+            tasks = Task.objects.filter(status=Task.STATUS_QUEUED).update(status=Task.STATUS_RETRY)
+            logger.info('Requeued {} tasks'.format(tasks))
+        except ProgrammingError:
+            logger.info('Not requeuing tasks')
+            pass
 
 
 class CqConfig(AppConfig):
@@ -40,7 +36,4 @@ class CqConfig(AppConfig):
     def ready(self):
         import cq.signals
         scan_tasks()
-        # Run requeue_tasks() only after migrations are run
-        post_migrate.connect(post_migration_callback, sender=self)
-        if not migration_happened:
-            connection_created.connect(requeue_tasks, sender=self)
+        requeue_tasks()
