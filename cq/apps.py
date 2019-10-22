136b37e22896fa1faf9e9eb16ce8cc71e4b7a0ad
Dean Mckendry <dean.mckendry91@gmail.com>
2019-10-22 13:17:37 +1100

    Increase reque timeout to avoid exception spam

--- cq/apps.py
+++ cq/apps.py
@@ -20,7 +20,7 @@ def scan_tasks(*args, **kwargs):
 def requeue_tasks(*args, **kwargs):
     from cq.models import Task
     lock = 'RETRY_QUEUED_TASKS'
-    with cache.lock(lock, timeout=2):
+    with cache.lock(lock, timeout=10):
         # Find all Queued tasks and set them to Retry, since they get stuck after a reboot
         try:
             tasks = Task.objects.filter(status=Task.STATUS_QUEUED).update(status=Task.STATUS_RETRY)
